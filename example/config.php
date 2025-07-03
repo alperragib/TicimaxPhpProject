9396882e20b6dc7bdc703ecf7254c418c6aa4a9a
Yusuf Kurnaz <146843816+yusufkrnz@users.noreply.github.com>
2025-07-03 16:15:47 +0300

    config.php

--- example/config.php
+++ example/config.php
@@ -2,6 +2,6 @@
 // Ticimax API config 
 
 return [
-    'mainDomain' => 'https://scentsan.com',
-    'apiKey' => 'A6CZ675NOFUZ6UXP17F61CEQJNYF79',
+    'mainDomain' => 'https://example.com',
+    'apiKey' => 'xxxxxxxxxxxxxxxxxx',
 ];
